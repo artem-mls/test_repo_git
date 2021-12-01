a826c15f067276fcffcec98a6c368046e74d70f6
Артем М <malyshev.ao@oksei.ru>
2021-12-01 19:39:16 +0500

    Add func plus(a, b)

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ def main():
     print('Hello, World!')
     print('WorldSkills Russia')
     print('PyCharm JetBrains')
+    print('PyCharm JetBrains')
+    print('PyCharm JetBrains')
+    print('PyCharm JetBrains')
+    print('PyCharm JetBrains')
 
 
 if __name__ == "__main__":
